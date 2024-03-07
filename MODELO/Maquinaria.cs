ce708b69c07e7004e62355ec737da1a02060a8ea
Andrea Alemán <103344222+AndreAG26@users.noreply.github.com>
2024-03-06 22:25:52 -0600

    :)

---
+++ MODELO/Maquinaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2.MODELO
+{
+    public abstract class Maquinaria: Comparable
+    {
+        protected double valor;
+        protected int vidaUtil;
+        protected String marca;
+        protected String descripcion;
+        protected Persona encargado;
+
+       public Maquinaria(double valor, int vidaUtil, string marca, string descricion)
+        {
+            this.valor = valor;
+            this.vidaUtil = vidaUtil;
+            this.marca = marca;
+            this.descricion = descricion;
+        }
+
+        public abstract String calcularDepreciacion();
+
+        public String getDescripcion() {return descripcion;}
+        public double getValor() {return valor; }
+
+
+        public String consultarNombreEncargado() 
+        {
+            return encargado.getNombre();
+        }
+
+        public bool menorQue(Comparable obj)
+        {
+            return this.valor<((Maquinaria)obj).getValor();
+        }
+
+        public void asignarEncargado(Persona obj) 
+        {
+            encargado = obj;
+        }
+    }
+}
---
+++ MODELO/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2.MODELO
+{
+    internal class Persona: Comparable
+    {
+        int id;
+        DateTime fechaNacimiento;
+        String nombre;
+
+        Persona (int id, DateTime fechaNacimiento, string nombre)
+        {
+            this.id = id;
+            this.fechaNacimiento = fechaNacimiento;
+            this.nombre = nombre;
+        }
+
+        public String getNombre()
+        {
+            return nombre;
+        }
+
+        public String consultarDepreciacion() //FUNCIÓN PENDIENTE
+        {
+            return "FUNCIÓN PENDIENTE";
+        }
+
+        public bool menorQue(Comparable obj) 
+        {
+            return String.Compare(this.getNombre(), ((Persona)obj).getNombre()) < 0;
+        }
+
+        public void asignarMaquinaria(Maquinaria obj) //FUNCIÓN PENDIENTE
+        {
+            if (obj == null) { }
+        }
+    }
+}
